a7accd5611aef96838b6dfc0b3066876c4d366b9
Panto1212 <patienceankundat@gmail.com>
2018-07-01 21:19:11 +0300

    add a loop to display numbers from one until input number on console

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
 //backend logic
-
-
-
-var pingpong = function(numb) {
+/*var pingpong = function(numb) {
   var divByThree = "Ping"
   var divByFive = "Pong"
-
   if (numb % 15 === 0) {
     return divByThree+divByFive;
   }
@@ -15,9 +11,35 @@ else if (numb % 5 === 0) {
   else if (numb % 3 === 0) {
     return divByThree;
   } else {
-    return false;
+    return numb;
   };
+};*/
+var pingpong = function(numb) {
+
+  var counter = 0;
+  while (counter < numb) {
+  counter++;
+
+var output = "";
+
+    if (counter % 15 === 0)
+      output += "PingPong";
+
+
+  else if (counter % 5 === 0)
+    output += "Pong";
+
+
+    else if (counter % 3 === 0)
+      output += "Ping";
+
+
+      console.log(output || counter);
+  };
+
 };
+
+
 
 
 
@@ -37,6 +59,6 @@ $(document).ready(function() {
     var input = parseInt($("input#num").val());
     var result = pingpong(input);
 
-$("#result").text(result);
+    $("#result").text(result);
   });
 });
